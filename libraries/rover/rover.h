360fe05c792050f24d1ca7308016338e72e7798a
eaman <eaman@pad.piffa.net>
2017-03-18 11:23:08 +0100

    lib base

--- libraries/rover/rover.h
+++ libraries/rover/rover.h
@@ -14,6 +14,39 @@ Licenza:    GPLv3
 #define rover_h
 
 //////////////////////
+// OBJ
+
+
+class engine {
+// Gestione di un singolo motore
+private:
+    byte enablePIN1;
+    byte enablePIN2;
+    byte min;   // Minimo per partire
+    byte val;   // 8bit PWM
+
+public:
+    Engine (byte PIN1, byte PIN2, byte PINPWM);
+    void Forward ();
+    void Backward ();
+    void SetSpeed (); // imposta un valore preciso per il PWM
+
+}
+
+class motors {
+// classe composta da due engines
+// Composition is better than inheritance here
+
+public:
+    motors (engine enginesx, engine enginedx) {
+    void Forward ();
+    void Backward ();
+    void TurnLeft ();
+    void TurnRight ();
+    void TurnU ();
+}
+}
+//////////////////////
 // Funzioni
 
 
