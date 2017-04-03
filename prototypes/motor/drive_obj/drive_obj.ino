d4f770ac3314d197b2563054a837093f17914dc4
Andrea Manni <andrea@piffa.net>
2017-04-03 14:03:35 +0200

    Primo metodo per drive ad oggetti

---
+++ prototypes/motor/drive_obj/drive_obj.ino
@@ -0,0 +1,50 @@
+/* L298n motor
+Aggiunta
+
+Guida 2WD composta da 2 motori
+
+- 2 motori DC
+- L298n module
+- Batteria > 6v
+
+*/
+
+#include <rover.h>
+
+void setup() {
+//    abilita();
+}
+
+car macchina ;
+
+void loop() {
+//// Avanti
+    macchina.avanti();
+//    digitalWrite(in1,LOW);
+//    digitalWrite(in2,HIGH);
+//    digitalWrite(enA,HIGH);
+    delay(2000);
+//// Stop
+//    car.stop();
+//    delay(1000);
+//
+//// TurnSX
+//    car.giraSX();
+//    delay(1000);
+//
+//// Indietro
+//    car.indietro();
+//    delay(2000);
+//// Stop
+//    car.stop();
+//    delay(1000);
+//
+//// TurnDX
+//    car.giraDX();
+//    delay(1000);
+//
+//// Stop
+//    car.stop();
+//    delay(1000);
+}
+
